a9139a7977021673a720e2e944852a8d0b73fce5
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-02-08 14:50:24 +0100

    Fix version constant name

--- setup.php
+++ setup.php
@@ -32,7 +32,7 @@
 define ('AIRWATCH_API_RESULT_OK',    'ok');
 define ('AIRWATCH_API_RESULT_ERROR', 'ko');
 define ('AIRWATCH_USER_AGENT', 'Airwatch-Connector-1.1');
-define ('AIRWATCH_PLUGIN_VERSION', '1.2.0');
+define ('PLUGIN_AIRWATCH_VERSION', '1.2.0');
 
 function plugin_init_airwatch() {
    global $PLUGIN_HOOKS,$CFG_GLPI,$LANG;
@@ -65,7 +65,7 @@ function plugin_version_airwatch() {
    global $LANG;
 
    return array ('name'           => __("GLPi Airwatch Connector", 'airwatch'),
-                   'version'        => AIRWATCH_PLUGIN_VERSION,
+                   'version'        => PLUGIN_AIRWATCH_VERSION,
                    'author'         => "<a href='http://www.teclib-edition.com'>Teclib'</a>",
                    'license'        => 'GPLv2+',
                    'homepage'       => 'https://github.com/pluginsglpi/airwatch',
