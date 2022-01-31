42dc3e5d61c833ad9c3f0c9d23a78845cff04f28
Cédric Anne <cedric.anne@gmail.com>
2022-01-31 09:36:16 +0100

    Fix licence headers

--- hook.php
+++ hook.php
@@ -1,33 +1,32 @@
 <?php
-/*
- * @version $Id$
- LICENSE
 
-  This file is part of the Airwatch plugin.
-
- Order plugin is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- Airwatch plugin is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with GLPI; along with Airwatch. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
- @package   airwatch
- @author    Teclib'
- @copyright Copyright (c) 2016 Teclib'
- @license   GPLv2+
-            http://www.gnu.org/licenses/gpl.txt
- @link      https://github.com/pluginsglpi/airwatch
- @link      http://www.glpi-project.org/
- @link      http://www.teclib-edition.com/
- @since     2016
- ---------------------------------------------------------------------- */
+/**
+ * -------------------------------------------------------------------------
+ * Airwatch plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Airwatch.
+ *
+ * Airwatch is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Airwatch is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Airwatch. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2016-2022 by Teclib'.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/airwatch
+ * -------------------------------------------------------------------------
+ */
 
 function plugin_airwatch_getAddSearchOptions($itemtype) {
 
